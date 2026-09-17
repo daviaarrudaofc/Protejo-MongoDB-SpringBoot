b93db73d461a9c7915dc740d59aa20c6f17973b3
davi arruda <daviarruda092@gmail.com>
2026-09-17 12:09:20 -0300

    Adiciona gitignore e Usando padrao DTO para retornar usuarios

--- src/main/java/com/daviarruda/workshopmongo/resources/UserResource.java
+++ src/main/java/com/daviarruda/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.daviarruda.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daviarruda.workshopmongo.domain.User;
+import com.daviarruda.workshopmongo.dto.UserDTO;
 import com.daviarruda.workshopmongo.services.UserService;
 
 @RestController
@@ -19,8 +21,13 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping    // Diz que este método vai responder requisições HTTP do tipo GET
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		// Converte a lista de User para uma lista de UserDTO.
+		// O stream() percorre cada User da lista,
+		// o map() transforma cada User em um UserDTO,
+		// e o collect(toList()) junta os DTOs em uma nova lista.
+		return ResponseEntity.ok().body(listDto);
 	}
 }
