89a52ac33954b904d32bc2a3b9523c6c6183032b
davi arruda <daviarruda092@gmail.com>
2026-09-18 16:54:07 -0300

    consulta simples com @Query

--- src/main/java/com/daviarruda/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/daviarruda/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,16 @@ package com.daviarruda.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.daviarruda.workshopmongo.domain.Post;
 
 
 public interface PostRepository extends MongoRepository<Post, String>{
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")//// opção alternativa de acordo com regex MongoDB
+	List<Post> searchByTitle(String text);
 
 }
